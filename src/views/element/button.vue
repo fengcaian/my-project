0670546bc385bf910fa2f38efe9579e790646a5a
fengcaian <caianfeng@yeah.net>
2019-02-10 18:49:37 +0800

    访问本地node服务成功

---
+++ src/views/element/button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="max-width: 1435px; overflow: auto">
+    <el-button type="primary" size="mini" @click="sendRequest">发送请求</el-button>
+  </div>
+</template>
+
+<script>
+// import sendAjax from '@/api'
+export default {
+  data () {
+    return {
+      responseData: ''
+    };
+  },
+  created () {
+    setTimeout(() => {
+      console.log(this);
+    }, 1000);
+  },
+  methods: {
+    sendRequest () {
+      this.axios.post('/getData.do')
+        .then((res) => {
+          console.log(res);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .text-overflow{
+    display:block;                     /*内联对象需加*/
+    word-break:keep-all;           /* 不换行 */
+    white-space:nowrap;          /* 不换行 */
+  }
+</style>
